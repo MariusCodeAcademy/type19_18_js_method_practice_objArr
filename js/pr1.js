3582c211b8a43dccf75b872de887e93bd77a3448
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-27 11:00:24 +0200

    select

--- js/pr1.js
+++ js/pr1.js
@@ -63,6 +63,8 @@ const students = [
 // taikomes
 const els = {
   studentsList: document.getElementById('students'),
+  allStdBtn: document.getElementById('btn1'),
+  siauliaiBtn: document.getElementById('btn2'),
 };
 console.log('els ===', els);
 // students.forEach((studObj) => {
@@ -71,19 +73,21 @@ console.log('els ===', els);
 
 // sugeneruoti id kiekvienam studentui
 
-// const stSuId = students.map((studObj, idx) => {
-//   // prideti dijanti id prie kiekvieno stud
-//   studObj.id = idx + 1;
-//   // idx++
-//   // idx = idx + 1
-//   return studObj;
-// });
+const stSuId = students.map((studObj, idx) => {
+  // prideti dijanti id prie kiekvieno stud
+  studObj.id = idx + 1;
+  // idx++
+  // idx = idx + 1
+  return studObj;
+});
 // console.log(JSON.stringify(stSuId, null, 1));
 console.table(students);
 // st1. sugeneruoti studentu nerikiuota sarasa htmle kuriame butu studentu vardas ir miestas
-function generateStudHtml() {
+function generateStudHtml(arr) {
+  // issivalyti konteineri
+  els.studentsList.innerHTML = '';
   // sukurti cikla ir jo viduje
-  students.forEach((studObj) => {
+  arr.forEach((studObj) => {
     // sukurti li
     const liEl = document.createElement('li');
     // prideti varda ir miesta
@@ -94,19 +98,47 @@ function generateStudHtml() {
     // 2. generateStudHtml() sudeda i html ne students elementus bet argumentu gauta masyva
   });
 }
-generateStudHtml();
+generateStudHtml(students);
 
 // st2. Parasyti funkcija, kuriai paduodam miesta kaip argumenta ir ji atrenka studentus is to miesto
 function getStudenstByTown(argTown) {
   const atrikti = students.filter((studObj) => studObj.town === argTown);
   console.table(atrikti);
   // 1. funkcija grazina atrinktus studentus
+  return atrikti;
 }
-getStudenstByTown('Klaipeda');
+const klaipedieciai = getStudenstByTown('Klaipeda');
+console.log('klaipedieciai ===', klaipedieciai);
 // 3. su generateStudHtml atspaudinti atrinktus studentus
+generateStudHtml(klaipedieciai);
+// generateStudHtml(klaipedieciai);
 //
+
+// Atspausdinti visus studentus su btn1
+els.allStdBtn.addEventListener('click', () => generateStudHtml(students));
+els.siauliaiBtn.addEventListener('click', () => {
+  // gauti std is Siauliu
+  const siaulieciai = getStudenstByTown('Siauliai');
+  // sugeneruoti html
+  generateStudHtml(siaulieciai);
+});
 //
 //
 //
 // st3. Parasyti funkcija, kuriai paduodam amziu kaip argumenta ir ji atrenka studentus kuriu amzius didesnis uz argumenta.
+function getStudAbove(amzius) {
+  // atrtinkti studentus kuriu amziu yra daugiau uz "amzius" reiksme
+  const atrinkti = students.filter((studObj) => {
+    console.log('studObj.age > amzius; ===', studObj.age > amzius);
+    return studObj.age > amzius;
+  });
+  console.log('atrinkti ===', atrinkti);
+  return atrinkti;
+}
+getStudAbove(25);
+
+// <select id="town">
+// kai ivyks miesto pasirinkimas
+// mes norim atspausdinti studentu is to miesto
+
 // st4. prie st1 sugeneruoto saraso pridedam mygtuka su textu 'delete'. mygtuka paspaudus istiname ta li el kuriame yra paspaustas mygtukas
